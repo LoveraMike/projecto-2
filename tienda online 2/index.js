cdad730895ad889b67938d482f867e2e68e2c09a
Server <server@server.com>
2023-08-12 16:25:26 -0300

    responsive done

--- tienda online 2/index.js
+++ tienda online 2/index.js
@@ -1,4 +1,15 @@
-window.addEventListener('scroll', function(){
-    var header = document.querySelector('header')
-    header.classList.toggle('abajo', window.scrollY>0)
-})
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById ('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active')
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active')
+    })
+}
